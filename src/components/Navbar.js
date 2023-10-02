36ad172dae598b96ec3365cdc6ec3e6fc3496d1a
FilbertKimasa <filbertdan67@gmail.com>
2023-10-02 17:44:48 +0300

    Managing State and Side-Effects

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useRef } from 'react';
+import { useOnClickOutside } from './useOnClickOutside';
+
+function Navbar() {
+  const [dropdown, setDropdown] = useState(false);
+  const ref = useRef();
+  console.log(ref);
+  useOnClickOutside(ref, dropdown, () => setDropdown(false));
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li ref={ref}>
+          <button onClick={() => setDropdown((prev) => !prev)}>
+            Services <span>&#8595;</span>
+          </button>
+          {dropdown && (
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './styles/app.css';
 import reactLogo from './images/react.svg';
 import TodoApp from './components/TodoApp';
-
+import Navbar from './components/Navbar';
+import Modal from './components/Modal';
 const rootElement = document.getElementById('root');
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
+    <Navbar />
     <TodoApp />
+    <Modal />
   </React.StrictMode>,
 );
